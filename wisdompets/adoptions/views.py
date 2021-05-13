b005687a1822fc130d8ec7b9d22bbdc86b1e2d84
AdamCarrera <anc170430@utdallas.edu>
2021-05-13 13:30:11 -0500

    initial commit

---
+++ wisdompets/adoptions/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<p>home view</p>')
+
+
+def pet_detail(request, pet_id):
+    return HttpResponse(f'<p>pet detail view with id {pet_id}</p>')
